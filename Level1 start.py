192d47f73a5d766842e8a24935159c6a8a8d369f
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-12 12:58:16 +1300

    Display match score

---
+++ Level1 start.py
@@ -0,0 +1,12 @@
+match_1 = 3
+match_2 = 6
+match_3 = 10
+match_4 = 10
+match_5 = 6
+match_total = match_1 + match_2 + match_3 + match_4 +match_5
+print("On match 1 they scored {}".format(match_1))
+print("On match 2 they scored {}".format(match_2))
+print("On match 3 they scored {}".format(match_3))
+print("On match 4 they scored {}".format(match_4))
+print("On match 5 they scored {}".format(match_5))
+print("In total they scored {}".format(match_total))
