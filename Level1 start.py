ff6e38853f5c4e7bb2617716ead3f2a8a2097913
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-12 13:11:55 +1300

    Fortnight purchases

--- Level1 start.py
+++ Level1 start.py
@@ -10,3 +10,14 @@ print("On match 3 they scored {}".format(match_3))
 print("On match 4 they scored {}".format(match_4))
 print("On match 5 they scored {}".format(match_5))
 print("In total they scored {}".format(match_total))
+
+skin = 10
+weapon = 10
+vbucks = 15
+giftcard_henry = 50
+print("Henry has {} on his gift card".format(giftcard_henry))
+giftcard_henry = giftcard_henry - skin - weapon - vbucks
+print("Henry spent {} on a skin, {} on a weapon and {} on some vbucks in fortnight".format(skin, weapon, vbucks))
+print("Henry has {} on his gift card".format(giftcard_henry))
+
+
