97d8025111e33ff5213b12063a694906439ff34b
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-13 10:33:10 +1300

    Named and spaced

--- Level1 start.py
+++ Level1 start.py
@@ -3,14 +3,17 @@ match_2 = 6
 match_3 = 10
 match_4 = 10
 match_5 = 6
-match_total = match_1 + match_2 + match_3 + match_4 +match_5
+match_total = match_1 + match_2 + match_3 + match_4 + match_5
+print("THIS PIECE OF WORK HAS NO NAME AND NO PURPOSE")
 print("On match 1 they scored {}".format(match_1))
 print("On match 2 they scored {}".format(match_2))
 print("On match 3 they scored {}".format(match_3))
 print("On match 4 they scored {}".format(match_4))
 print("On match 5 they scored {}".format(match_5))
 print("In total they scored {}".format(match_total))
+print("")
 
+print("THIS PIECE OF WORK ALSO HAS NO NAME AND NO PURPOSE")
 skin = 10
 weapon = 10
 vbucks = 15
