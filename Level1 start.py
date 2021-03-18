e919bc569e6f69555fe7c140bf8722566728d7bb
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-19 12:35:54 +1300

    Fortnight purchases

--- Level1 start.py
+++ Level1 start.py
@@ -7,7 +7,6 @@ match_5 = 6
 #creates totals
 match_total = match_1 + match_2 + match_3 + match_4 + match_5
 #prints results
-print("THIS PIECE OF WORK HAS NO NAME AND NO PURPOSE")
 print("On match 1 they scored {}".format(match_1))
 print("On match 2 they scored {}".format(match_2))
 print("On match 3 they scored {}".format(match_3))
@@ -16,7 +15,6 @@ print("On match 5 they scored {}".format(match_5))
 print("In total they scored {}".format(match_total))
 print("")
 
-print("THIS PIECE OF WORK ALSO HAS NO NAME AND NO PURPOSE")
 skin = 10
 weapon = 10
 vbucks = 15
