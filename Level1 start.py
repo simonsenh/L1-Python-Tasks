fdf7752d287e3d0265994566dc6b402985607c8f
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-19 13:04:19 +1300

    Fortnight purchases

--- Level1 start.py
+++ Level1 start.py
@@ -21,7 +21,7 @@ vbucks = 15
 giftcard_henry = 50
 print("Henry has {} on his gift card".format(giftcard_henry))
 giftcard_henry = giftcard_henry - skin - weapon - vbucks
-print("Henry spent {} on a skin, {} on a weapon and {} on some vbucks in fortnight".format(skin, weapon, vbucks))
+print("Henry spent {} on a skin, {} on a weapon and {} on some vbucks in fortnite".format(skin, weapon, vbucks))
 print("Henry has {} on his gift card".format(giftcard_henry))
 
 
