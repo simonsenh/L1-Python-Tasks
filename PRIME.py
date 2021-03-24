7672b4b5e9844bd06e347ccb9b2335226da1d279
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-25 09:13:39 +1300

    prime sucsess

--- PRIME.py
+++ PRIME.py
@@ -1,4 +1,4 @@
-b = ("")
+b = 1
 a = 2
 c = 1
 n = int(input("What is your number"))
@@ -7,8 +7,12 @@ while c <= n:
     a = 2
     while a <= n:
         if c * a == n:
-            b = ("not")
+            b = 2
         a = a + 1
 if n == 1:
-    b = ("not")
-print("It is {} a prime number".format(b))
+    b = 2
+if b == 2:
+   print("It is not a prime number")
+if b == 1:
+   print("It is a prime number")
+#This code will only go up to around 4 digits numbers any higher and the code will take longer than 15 seconds.
