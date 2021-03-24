8d0cbd9d8e969ec3827aca4636375da80330dc49
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-25 08:58:45 +1300

    prime sucsess

--- PRIME.py
+++ PRIME.py
@@ -1,8 +1,14 @@
 b = ("")
 a = 2
+c = 1
 n = int(input("What is your number"))
-while a == n - 1:
-    if n / a == int:
-        b = ("not")
-    a = a + 1
+while c <= n:
+    c += 1
+    a = 2
+    while a <= n:
+        if c * a == n:
+            b = ("not")
+        a = a + 1
+if n == 1:
+    b = ("not")
 print("It is {} a prime number".format(b))
