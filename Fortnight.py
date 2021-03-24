17556444e39cb4dc9e76e2aaf362108d646664e0
simonsenh <simonsenh@stu.otc.school.nz>
2021-03-25 09:35:35 +1300

    Fortnight purchases

---
+++ Fortnight.py
@@ -0,0 +1,8 @@
+skin = 10
+weapon = 10
+vbucks = 15
+giftcard_henry = 50
+print("Henry has {} on his gift card".format(giftcard_henry))
+giftcard_henry = giftcard_henry - skin - weapon - vbucks
+print("Henry spent {} on a skin, {} on a weapon and {} on some vbucks in fortnite".format(skin, weapon, vbucks))
+print("Henry has {} on his gift card".format(giftcard_henry))
