f2ffe3031c1c6ed1739df8462f9551e27fd0e97c
simonsenh <simonsenh@stu.otc.school.nz>
2021-04-08 15:33:11 +1200

    Added Comments

--- sum1ton 1.py
+++ sum1ton 1.py
@@ -1,3 +1,19 @@
-#sum1ton 1
-n = int(input("What is your number"))
-print((n + 1) *(n / 2))
+
+#sum1ton 1 by henry simonsen on 1/3/2021
+
+#Gets number from user
+try:
+    n=int(input("What is your number (positive integer)? "))
+    #test int is positive
+    if n > 0:
+        #Outputs sum of all numbers under the choosen number
+        print((n + 1) * (n / 2))
+    else:
+        print("Not a negative!!!!!!")
+except ValueError:
+    print("Please input a number between integer >0 ")
+
+
+
+
+
