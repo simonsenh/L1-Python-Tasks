910c91d63a75b0f82f1d419d6a1c1fd77c956b67
simonsenh <simonsenh@stu.otc.school.nz>
2021-04-15 09:11:28 +1200

    List1

---
+++ Name list.py
@@ -0,0 +1,14 @@
+name_list = ["Evi", "Madeleine", "Dan", "Kelsey", "Cayden", "Hayley", "Darian"]
+print("The list of names is {}".format(name_list))
+print("The list of names in alphabetical order {}".format(sorted(name_list)))
+user_name = input("What is your name?")
+if user_name in name_list:
+    print("Your name is in the list!!!")
+else:
+    print("Your name is not in the list")
+print("The list of names is {}".format(name_list))
+Replace_name = input("What name would you like to replace?")
+if Replace_name in name_list:
+    Replace
+else:
+    print("This is not a name in the list!!!!")
