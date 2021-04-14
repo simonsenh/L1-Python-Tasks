1852fee73cdcb50799e2160bad2ea4ee05b31563
simonsenh <simonsenh@stu.otc.school.nz>
2021-04-15 09:39:21 +1200

    sAD FACE

--- Name list.py
+++ Name list.py
@@ -3,7 +3,7 @@ print("The list of names is {}".format(name_list))
 print("The list of names in alphabetical order {}".format(sorted(name_list)))
 user_name = input("What is your name?")
 if user_name in name_list:
-    print("Your name is in the list!!!")
+    print("Your name is in the list!!!:(")
 else:
     print("Your name is not in the list")
     Replace_name = input("What name would you like to replace?")
@@ -15,4 +15,4 @@ else:
       name_list.append(new_name)
       print(name_list)
     else:
-     print("This is not a name in the list!!!!")
+     print("This is not a name in the list!!!:(")
