d8f5905b560fe2063617e3c87475a0c5ef347eab
simonsenh <simonsenh@stu.otc.school.nz>
2021-04-15 09:29:09 +1200

    List done

--- Name list.py
+++ Name list.py
@@ -6,9 +6,13 @@ if user_name in name_list:
     print("Your name is in the list!!!")
 else:
     print("Your name is not in the list")
-print("The list of names is {}".format(name_list))
-Replace_name = input("What name would you like to replace?")
-if Replace_name in name_list:
-    Replace
-else:
-    print("This is not a name in the list!!!!")
+    Replace_name = input("What name would you like to replace?")
+    if Replace_name in name_list:
+      place = name_list.index(Replace_name)
+      name_list[place] = user_name
+      print(name_list)
+      new_name = input("What name would you like to add?")
+      name_list.append(new_name)
+      print(name_list)
+    else:
+     print("This is not a name in the list!!!!")
