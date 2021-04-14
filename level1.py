4760d8e62c83853c56d2e11451c4f815c129fe53
simonsenh <simonsenh@stu.otc.school.nz>
2021-04-15 08:52:46 +1200

    Fixed??

--- level1.py
+++ level1.py
@@ -1,16 +1,8 @@
-#int variable
-match_1 = 3
-match_2 = 6
-match_3 = 10
-match_4 = 10
-match_5 = 6
-#creates totals
-match_total = match_1 + match_2 + match_3 + match_4 + match_5
-#prints results
-print("On match 1 they scored {}".format(match_1))
-print("On match 2 they scored {}".format(match_2))
-print("On match 3 they scored {}".format(match_3))
-print("On match 4 they scored {}".format(match_4))
-print("On match 5 they scored {}".format(match_5))
-print("In total they scored {}".format(match_total))
-print("")
+skin = 10
+weapon = 10
+vbucks = 15
+giftcard_henry = 50
+print("Henry has {} on his gift card".format(giftcard_henry))
+giftcard_henry = giftcard_henry - skin - weapon - vbucks
+print("Henry spent {} on a skin, {} on a weapon and {} on some vbucks in fortnite".format(skin, weapon, vbucks))
+print("Henry has {} on his gift card".format(giftcard_henry))
