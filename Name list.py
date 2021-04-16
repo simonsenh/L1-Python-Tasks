c07f2b79b54e1faacf1a933148cc74e97147449c
simonsenh <simonsenh@stu.otc.school.nz>
2021-04-16 12:56:30 +1200

    SPACE

--- Name list.py
+++ Name list.py
@@ -1,17 +1,17 @@
 name_list = ["Evi", "Madeleine", "Dan", "Kelsey", "Cayden", "Hayley", "Darian"]
 print("The list of names is {}".format(name_list))
 print("The list of names in alphabetical order {}".format(sorted(name_list)))
-user_name = input("What is your name?")
+user_name = input("What is your name? ")
 if user_name in name_list:
     print("Your name is in the list!!!:(")
 else:
     print("Your name is not in the list")
-    Replace_name = input("What name would you like to replace?")
+    Replace_name = input("What name would you like to replace? ")
     if Replace_name in name_list:
       place = name_list.index(Replace_name)
       name_list[place] = user_name
       print(name_list)
-      new_name = input("What name would you like to add?")
+      new_name = input("What name would you like to add? ")
       name_list.append(new_name)
       print(name_list)
     else:
